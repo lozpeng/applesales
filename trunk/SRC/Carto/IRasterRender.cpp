d1a0ce0409eb51277ff7ca276299f8b16e672e53
19830828zhh@163.com <19830828zhh@163.com@88a054c9-7ac1-712f-8f31-7815adbf56d5>
2010-11-29 13:23:53 +0000

    hhzhao,添加IRasterRenderer

---
+++ trunk/SRC/Carto/IRasterRender.cpp
@@ -0,0 +1,98 @@
+#include "stdafx.h"
+#include "IRasterRender.h"
+//#include "RasterRGBRender.h"
+
+namespace Carto
+{
+
+IRasterRender::IRasterRender()
+{
+    m_pRasterDataset =NULL;
+	m_extRenderName = "";
+}
+IRasterRender::~IRasterRender()
+{
+
+}
+
+void IRasterRender::serialization(SYSTEM::IArchive &ar)
+{
+	SYSTEM::ISerialization::serialization( ar );
+
+	int type;
+
+	if(ar.IsRead() )
+	{
+		ar & type;
+
+		//OTASSERT( m_type == (otCarto::RASTER_RENDER_TYPE)type );
+		m_type == (Carto::RASTER_RENDER_TYPE)type;
+		m_type = (Carto::RASTER_RENDER_TYPE)type;
+	}
+	else
+	{
+		type = (int)m_type;
+		ar & type;
+	}
+}
+
+SYSTEM::CSmartPtr<IRasterRender> IRasterRender::CreateRender(Carto::RASTER_RENDER_TYPE type, std::string RenderExtName )
+{
+	//switch(type)
+	//{
+	//case RASTER_RGBRENDER:
+	//	return IRasterRenderPtr( new CRasterRGBRender() );
+	//case RASTER_CUSTOMRENDER: 
+	//	return NULL;
+	//default:
+		return NULL;
+	//}
+}
+
+SYSTEM::CSmartPtr<IRasterRender> IRasterRender::CreateRenderFromStream(SYSTEM::IArchive &ar)
+{
+	
+	int startpos = ar.GetCurrentPos();
+	int type;
+	ar & type;
+
+	std::string extRenderName = "";
+
+	if (type == RASTER_CUSTOMRENDER)
+	{
+		ar & extRenderName;
+	}
+
+	IRasterRenderPtr pRender = CreateRender( (RASTER_RENDER_TYPE)type, extRenderName);
+
+	ar.GetCurrentPos() = startpos; 
+
+	if (pRender != NULL)
+		pRender->serialization( ar );
+
+	return pRender;
+}
+
+void IRasterRender::ApplyEnhanceDraw(const int nEnhanceMode)
+{
+	return;
+}
+
+void IRasterRender::ApplyPaletteDraw(BYTE *pbRed, BYTE *pbGreen, BYTE *pbBlue)
+{
+	return;
+}
+
+void IRasterRender::SetAffineTransform(CAffineTransformPtr affineTransPtr)
+{
+	assert(affineTransPtr != NULL);
+	m_pAffineTransform = affineTransPtr;
+
+}
+
+CLegendInfoPtr IRasterRender::GetLegendInfo()
+{
+	return NULL;
+}
+
+} //namespace otCarto
---
+++ trunk/SRC/Carto/IRasterRender.h
@@ -0,0 +1,98 @@
+#ifndef _IRASTER_RENDER_h
+#define _IRASTER_RENDER_h
+
+#include "IDisplay.h"
+#include "LegendInfo.h"
+
+namespace Geodatabase
+{
+	class IRasterDataset;
+}
+
+namespace Carto
+{
+
+class  CARTO_DLL IRasterRender : public SYSTEM::ISerialization
+{
+public:
+	IRasterRender();
+	virtual ~IRasterRender();
+
+public:
+
+	/**
+	* 对选中的记录集进行绘制
+	* @param pDisplay 智能指针，指向显示接口
+	* @param pRaster 栅格数据源
+	*/
+	virtual void Draw( Display::IDisplayPtr pDisplay,Geodatabase::IRasterDataset* pRaster) = 0;
+
+	/**
+	* 对当前专题进行序列化操作
+	* @param ar 文档流
+	*/
+	virtual void serialization(SYSTEM::IArchive &ar);
+
+	/**
+	* 根据流生成专题
+	* @param ar 文档流
+	* @return 返回专题
+	*/
+	static SYSTEM::CSmartPtr<IRasterRender> CreateRenderFromStream(SYSTEM::IArchive &ar);
+
+	/**
+	* 创建专题
+	* @param type 专题类型
+	* @param RenderExtName 扩展专题名
+	* @return 返回专题
+	*/
+	static SYSTEM::CSmartPtr<IRasterRender> CreateRender( RASTER_RENDER_TYPE type , std::string RenderExtName = "" );
+
+
+	/**
+	* 对当前Render中的栅格数据源增强显示绘制
+	* @param nEnhanceMode 增强显示方法
+	*/
+	virtual void ApplyEnhanceDraw(const int nEnhanceMode);
+
+	/** 
+	* 对当前Render中的栅格数据源应用调色板显示
+	* @param pbRed 红色通道映射值
+	* @param pbGreen 绿色通道映射值
+	* @param pbBlue 蓝色通道映射值
+	*/
+	virtual void ApplyPaletteDraw(BYTE *pbRed, BYTE *pbGreen, BYTE *pbBlue);
+
+
+	virtual void SetAffineTransform(CAffineTransformPtr affineTransPtr);
+
+	/*
+	* 得到图例的信息
+	*/
+	virtual CLegendInfoPtr GetLegendInfo();
+
+protected:
+
+	
+	RASTER_RENDER_TYPE m_type;
+
+	std::string m_extRenderName;
+
+
+	//渲染的栅格数据源
+	Geodatabase::IRasterDataset* m_pRasterDataset;
+
+	//用于动态投影
+	CAffineTransformPtr m_pAffineTransform;
+
+
+};
+
+typedef SYSTEM::CSmartPtr<IRasterRender> IRasterRenderPtr;
+
+
+	
+
+}
+
+#endif
