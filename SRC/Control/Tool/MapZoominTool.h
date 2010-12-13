14f1d0cb564116af289fb3b5d65ec31dc24f7c67
lordfm <lordfm@163.com>
2010-12-13 14:01:00 +0000

    加入放大缩小工具

--- SRC/Control/Tool/MapZoominTool.h
+++ SRC/Control/Tool/MapZoominTool.h
@@ -2,10 +2,10 @@
 // 版权(c) 2010-2020, 天地智绘
 // 作者：  leo
 // 时间：  2010.12
-// 描述：  地图漫游工具
+// 描述：  地图放大工具
 //////////////////////////////////////////////////////////////////////
-#ifndef _MapPanTool_H
-#define _MapPanTool_H
+#ifndef _MapZoominTool_H
+#define _MapZoominTool_H
 
 #include "ITool.h"
 #include "IMapCtrl.h"
@@ -13,25 +13,20 @@
 namespace Control
 {
 
-class  CToolMapPan: public Framework::ITool
+class  CToolMapZoomin: public Framework::ITool
 {
 public:
-	CToolMapPan();
-	~CToolMapPan();
+	CToolMapZoomin();
+	~CToolMapZoomin();
 
 public:
 	void Initialize(Framework::IUIObject *pTargetControl);
 	void LButtonDownEvent (UINT nFlags, CPoint point);
-	void MouseMoveEvent (UINT nFlags, CPoint point);
-	void LButtonUpEvent (UINT nFlags, CPoint point);
 
 private:
-	CPoint cPt0,cPt1,cPt2;
-
-	Framework::IMapCtrl *m_pMapCtrl;
-
 	HCURSOR m_hCursor;
 
+    Framework::IMapCtrl *m_pMapCtrl;
 
 };
 
