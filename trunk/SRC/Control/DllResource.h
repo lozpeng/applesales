3390e689c09b0e9533e2d99d1028421bfcef0d4f
lordfm@163.com <lordfm@163.com@88a054c9-7ac1-712f-8f31-7815adbf56d5>
2011-01-06 13:55:57 +0000

    加入MapTree类，初步修改，还需要完善

---
+++ trunk/SRC/Control/DllResource.h
@@ -0,0 +1,19 @@
+#ifndef _DLL_RESOURCE_H_
+#define _DLL_RESOURCE_H_
+
+namespace Control
+{
+
+class CDllResource
+{
+public:
+	CDllResource(void);
+	~CDllResource(void);
+protected:
+	HINSTANCE m_hOldInst;
+};
+
+}
+#endif
+
+
