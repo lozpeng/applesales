a10dc29669e3732353799225329f1ed5e6c86b9b
lordfm@163.com <lordfm@163.com@88a054c9-7ac1-712f-8f31-7815adbf56d5>
2011-03-19 05:37:56 +0000

    添加了图层基本属性

---
+++ trunk/SRC/Control/UI/LayerPropSheet.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ILayer.h"
+
+class CLayerPropSheet : public CPropertySheet
+{
+	DECLARE_DYNAMIC(CLayerPropSheet)
+public:
+	CLayerPropSheet(CString name , CWnd *pWnd = NULL);
+	~CLayerPropSheet(void);
+public:
+
+	virtual INT_PTR DoModal();
+
+	void SetLayer(Carto::ILayerPtr layer);
+	
+public:
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnApply();
+
+
+	virtual BOOL OnInitDialog();
+private:
+	Carto::ILayerPtr m_layer;
+
+};
