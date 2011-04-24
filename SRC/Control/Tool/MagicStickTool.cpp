fbc3aed3559f79952958d4c2aa76bb1bb3c89ece
lordfm <lordfm@163.com>
2011-04-24 10:10:45 +0000

    魔术棒工具加入默认鼠标光标

--- SRC/Control/Tool/MagicStickTool.cpp
+++ SRC/Control/Tool/MagicStickTool.cpp
@@ -30,20 +30,14 @@ void CMagicStickTool::Initialize(Framework::IUIObject *pTargetControl)
 {
 	ITool::Initialize(pTargetControl);
 
-	//初始化光标
-	if(m_hCursor==NULL)
-	{
-		//m_hCursor =::LoadCursor( theApp.m_hInstance , MAKEINTRESOURCE( IDC_ZOOMIN) );
-	}
-
-
+	
 	//获取活动地图控件
 	m_pMapCtrl = dynamic_cast<Framework::IMapCtrl*>(pTargetControl);
 	if(!m_pMapCtrl)
 		return;
 
 	//设置光标类型
-	//m_pMapCtrl->SetCursor(m_hCursor);
+	m_pMapCtrl->SetCursor(m_hCursor);
 
 
 }
