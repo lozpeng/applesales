3d76a611f6ea1c2f784ecf3e0679dea2fe6bf0c7
lordfm@163.com <lordfm@163.com@88a054c9-7ac1-712f-8f31-7815adbf56d5>
2011-05-05 14:57:20 +0000

    加入变化检测对话框

---
+++ trunk/SRC/Control/DlgChangeDetect.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "afxwin.h"
+
+
+// CDlgChangeDetect 对话框
+
+class CDlgChangeDetect : public CDialog
+{
+	DECLARE_DYNAMIC(CDlgChangeDetect)
+
+public:
+	CDlgChangeDetect(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CDlgChangeDetect();
+
+// 对话框数据
+	enum { IDD = IDD_IMG_CHANGEDETECT };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CComboBox m_CmbSrc;
+	CComboBox m_CmbDest;
+	CString m_strResult;
+	BOOL m_bLoadShp;
+	afx_msg void OnBnClickedBtnSelchashp();
+	afx_msg void OnBnClickedOk();
+	virtual BOOL OnInitDialog();
+};
