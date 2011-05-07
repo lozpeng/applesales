3c9de81e650586a72e75bd845ff71ac34f1c2a63
lordfm <lordfm@163.com>
2011-05-07 14:52:14 +0000

    加入创建影像的函数

--- SRC/DataSourceRaster/RasterWorkspace.cpp
+++ SRC/DataSourceRaster/RasterWorkspace.cpp
@@ -52,9 +52,59 @@ IRasterDatasetPtr CRasterWorkspace::OpenRasterDataset(const char *name, bool Rea
 bool CRasterWorkspace::CreateRasterDataset(const char *name,RasFileInfo *fileInfo)
 {
 
+	GDALDataType datatype;
+	BANDDATATYPE ldatatype=(BANDDATATYPE)fileInfo->lDataType;
+	if(BDT_BYTE==ldatatype)
+	{
+		datatype=GDT_Byte;
+	}
+	else if(BDT_USHORT==ldatatype)
+	{
+		datatype=GDT_UInt16;
+	}
+	else if(BDT_SHORT==ldatatype)
+	{
+		datatype=GDT_Int16;
+	}
+	else if(BDT_FLOAT==ldatatype)
+	{
+		datatype=GDT_Float32;
+	}
+	else if(BDT_DOUBLE==ldatatype)
+	{
+		datatype=GDT_Float64;
+	}
+	//获取文件名后缀
+	string ext =name;
 
-	return false;
+	ext =ext.substr(ext.rfind('.')+1,ext.size()-ext.rfind('.')-1);
 
+	GDALDriver *poDriver=NULL;
+	if(stricmp(ext.c_str(),"tif")==0 || stricmp(ext.c_str(),"tiff")==0)
+	{
+		poDriver =GetGDALDriverManager()->GetDriverByName("GTiff");
+	}
+	else if(stricmp(ext.c_str(),"jpg")==0)
+	{
+		poDriver =GetGDALDriverManager()->GetDriverByName("JPEG");
+	}
+	else if(stricmp(ext.c_str(),"bmp")==0)
+	{
+		poDriver =GetGDALDriverManager()->GetDriverByName("BMP");
+	}
+	if(!poDriver)
+	{
+		return false;
+	}
+	
+	GDALDataset *poDstDS=0;
+	poDstDS=poDriver->Create(name,fileInfo->lWidth,fileInfo->lHeight,fileInfo->lChannelNum,datatype,0);
 
+	if(poDstDS ==NULL)
+	{
+		return false;
+	}
+	delete poDstDS;
+	return true;
 
 }
