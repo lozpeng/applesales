55977aa34568d9f6f11eec1d5e714b00b54476d7
lordfm <lordfm@163.com>
2011-05-16 14:07:08 +0000

    变化检测的结果，符号设置为了空心

--- SRC/Control/UI/ImageProcessTool.cpp
+++ SRC/Control/UI/ImageProcessTool.cpp
@@ -4,6 +4,11 @@
 #include "DlgChangeDetect.h"
 #include "ImgChangeDetect.h"
 #include "IDocument.h"
+#include "FeatureLayer.h"
+#include "SimpleRender.h"
+#include "IMapCtrl.h"
+#include "ShapefileWorkspaceFactory.h"
+#include "IWorkspace.h"
 namespace Control
 {
 
@@ -21,7 +26,39 @@ void CImageProcessTool::ShowImgChangeDetectDlg()
 			 if(dlg.m_bLoadShp)
 			 {
                  Framework::IDocument *pDoc =(Framework::IDocument*)Framework::IUIObject::GetUIObjectByName(Framework::CommonUIName::AppDocument);
-				 pDoc->LoadShpFile(dlg.m_strResult);
+				 //pDoc->LoadShpFile(dlg.m_strResult);
+
+				 CString csDataSourceTmp=dlg.m_strResult;
+
+				 CString csThemeName = csDataSourceTmp.Mid (csDataSourceTmp.ReverseFind ('\\') + 1);
+				 csThemeName =csThemeName.Left(csThemeName.ReverseFind('.'));
+
+				 Geodatabase::IWorkspace* ipWorkspace = CShapefileWorkspaceFactory::GetInstance()->OpenFromFile(dlg.m_strResult);
+				 Geodatabase::IFeatureClassPtr ipFeatureCls = ipWorkspace->OpenFeatureClass(dlg.m_strResult);
+
+
+				 Carto::ILayerPtr pLayer = Carto::ILayerPtr(new Carto::CFeatureLayer());
+				 pLayer = Carto::ILayer::CreateLayer(ipFeatureCls);
+				 //ÉèÖÃÍ¼²ãÃû
+				 pLayer->SetName(std::string(csThemeName));
+
+				 Carto::IFeatureLayerPtr pFeatureLayer =pLayer;
+				 Carto::CSimpleRenderPtr pRender =pFeatureLayer->GetFeatureRender();
+
+				 //ÉèÖÃ·ûºÅ
+				 Display::CSimpleFillSymbolPtr pFillSymbol =pRender->GetSymbol();
+				 pFillSymbol->SetDrawFill(false);
+				 pFillSymbol->SetOutLineWidth(1.5);
+				 pFillSymbol->SetOutLineColor(RGB(255,0,0));
+
+				 pDoc->GetActiveMap()->AddLayer(pLayer);
+
+				 pDoc->GetLinkMapTree()->AddLayer(pLayer);
+				 
+
+				 
+
+				 
 			 }
 		 }
 		 else
