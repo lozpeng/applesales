88f00689307b920dabd8484a55b981574fc01f75
whuhhz <whuhhz@gmail.com>
2011-06-26 10:20:23 +0000

    图元增量导出，修改完成

---
+++ SRC/TDApp/DrawingIncrExport.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "afxwin.h"
+
+
+// CDrawingIncrExport 对话框
+
+class CDrawingIncrExport : public CDialog
+{
+	DECLARE_DYNAMIC(CDrawingIncrExport)
+
+public:
+	CDrawingIncrExport(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CDrawingIncrExport();
+
+// 对话框数据
+	enum { IDD = IDD_DIG_Drawing_IncrExport };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+
+
+	BOOL OnInitDialog();
+public:
+	CComboBox m_ComboxType;
+	BOOL m_bExpoertAll;
+	CComboBox m_Combo_TagertLayer;
+	CString m_ExportPath;
+	CString m_TargetLayerName;
+	int m_DrawingType;
+	afx_msg void OnBnClickedBtnSlectpath();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnCbnSelendcancelComboDrawingtype();
+	afx_msg void OnBnClickedRadioExportAll();
+	afx_msg void OnBnClickedRadioExpoertSelected();
+	afx_msg void OnCbnSelchangeComboDrawingtype();
+};
