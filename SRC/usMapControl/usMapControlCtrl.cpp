842b9424ec7c856093a1fc34a766dfdf7ef5bca8
lordfm <lordfm@163.com>
2011-11-22 14:23:04 +0000

    加入地图控件的工具属性

--- SRC/usMapControl/usMapControlCtrl.cpp
+++ SRC/usMapControl/usMapControlCtrl.cpp
@@ -10,6 +10,7 @@
 #include "FeatureLayer.h"
 #include "RasterLayer.h"
 #include "RasterWSFactory.h"
+#include "ActivexLoad.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -27,6 +28,14 @@ BEGIN_MESSAGE_MAP(CusMapCtrl, COleControl)
 	ON_WM_CREATE()
 	ON_WM_PAINT()
 	ON_WM_SIZE()
+	ON_WM_LBUTTONDBLCLK()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_MOUSEWHEEL()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_TIMER()
 END_MESSAGE_MAP()
 
 
@@ -37,6 +46,7 @@ BEGIN_DISPATCH_MAP(CusMapCtrl, COleControl)
 	DISP_FUNCTION_ID(CusMapCtrl, "AboutBox", DISPID_ABOUTBOX, AboutBox, VT_EMPTY, VTS_NONE)
 	DISP_FUNCTION_ID(CusMapCtrl, "AddShpfile", dispidAddShpfile, AddShpfile, VT_EMPTY, VTS_BSTR)
 	DISP_FUNCTION_ID(CusMapCtrl, "Refresh", dispidRefresh, Refresh, VT_EMPTY, VTS_NONE)
+	DISP_PROPERTY_EX_ID(CusMapCtrl, "CurTool", dispidCurTool, GetCurTool, SetCurTool, VT_I2)
 END_DISPATCH_MAP()
 
 
@@ -130,6 +140,12 @@ CusMapCtrl::CusMapCtrl()
 	CreateAss("USMAPCONTROL");
 
 	SetAutoDetroy(true);
+
+	m_bActiveX =true;
+
+	m_pCurTool =NULL;
+
+	m_Tooltype =NoneTool;
 }
 
 
@@ -213,6 +229,9 @@ int CusMapCtrl::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 	Framework::IMapCtrl::SetMap(ptrMap);
 
+	//为了能够加载Control库，所以调用一个没有作用的类
+	Control::CActivexLoad cLoad;
+
 	return 0;
 }
 
@@ -293,4 +312,127 @@ void CusMapCtrl::Refresh(void)
 	AFX_MANAGE_STATE(AfxGetStaticModuleState());
 
 	UpdateControl(drawAll);
+}
+
+void CusMapCtrl::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	if(m_pCurTool)
+	{
+		m_pCurTool->LButtonDblClkEvent(nFlags,point);
+	}
+	__super::OnLButtonDblClk(nFlags, point);
+}
+
+void CusMapCtrl::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	if(m_pCurTool)
+	{
+		m_pCurTool->LButtonDownEvent(nFlags,point);
+	}
+
+	__super::OnLButtonDown(nFlags, point);
+}
+
+void CusMapCtrl::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	if(m_pCurTool)
+	{
+		m_pCurTool->LButtonUpEvent(nFlags,point);
+	}
+
+	__super::OnLButtonUp(nFlags, point);
+}
+
+void CusMapCtrl::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if(m_pCurTool)
+	{
+		m_pCurTool->MouseMoveEvent(nFlags,point);
+	}
+
+	__super::OnMouseMove(nFlags, point);
+}
+
+BOOL CusMapCtrl::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	return __super::OnMouseWheel(nFlags, zDelta, pt);
+}
+
+void CusMapCtrl::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	if(m_pCurTool)
+	{
+		m_pCurTool->RButtonDownEvent(nFlags,point);
+	}
+
+	__super::OnRButtonDown(nFlags, point);
+}
+
+void CusMapCtrl::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	
+	__super::OnRButtonUp(nFlags, point);
+}
+
+void CusMapCtrl::OnTimer(UINT_PTR nIDEvent)
+{
+	
+
+	__super::OnTimer(nIDEvent);
+}
+
+usToolType CusMapCtrl::GetCurTool(void)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	
+
+	return m_Tooltype;
+}
+
+void CusMapCtrl::SetCurTool(usToolType newVal)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+
+	m_Tooltype =newVal;
+
+	m_pCurTool =GetTool(newVal);
+
+	if(m_pCurTool)
+	{
+		m_pCurTool->Initialize(dynamic_cast<Framework::IUIObject*>(this));
+	}
+
+	SetModifiedFlag();
+}
+
+Framework::ITool* CusMapCtrl::GetTool(usToolType tooltype)
+{
+	if(NoneTool==tooltype)
+	{
+		return NULL;
+	}
+
+	Framework::ITool *pTool =NULL;
+	switch(tooltype)
+	{
+	case PanTool:
+		pTool =Framework::ITool::CreateTool("MapPan");
+		break;
+	case ZoomInTool:
+		pTool =Framework::ITool::CreateTool("MapZoomin");
+		break;
+	case ZoomOutTool:
+		pTool =Framework::ITool::CreateTool("MapZoomout");
+		break;
+	default:
+		break;
+
+	}
+
+	return pTool;
 }
--- SRC/usMapControl/usMapControlCtrl.h
+++ SRC/usMapControl/usMapControlCtrl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "InternalControl.h"
-// usMapControlCtrl.h : CusMapCtrl ActiveX 控件类的声明。
+#include "ICommand.h"
+#include "ITool.h"
 
 
 // CusMapCtrl : 有关实现的信息，请参阅 usMapControlCtrl.cpp。
@@ -42,6 +43,7 @@ protected:
 // 调度和事件 ID
 public:
 	enum {
+		dispidCurTool = 3,
 		dispidRefresh = 2L,
 		dispidAddShpfile = 1L
 
@@ -54,11 +56,28 @@ public:
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	afx_msg void OnPaint();
 	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
 protected:
 	void AddShpfile(LPCTSTR filename);
 	void Refresh(void);
 
 private:
-	//CInternalControl m_MapControl;
+	Framework::ITool* GetTool(usToolType tooltype);
+
+private:
+	//当前工具
+	Framework::ITool* m_pCurTool;
+
+	usToolType m_Tooltype;
+protected:
+	usToolType GetCurTool(void);
+	void SetCurTool(usToolType newVal);
 };
 
