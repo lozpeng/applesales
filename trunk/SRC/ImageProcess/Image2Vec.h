da5cc76661b33dc7c2e285bc9a036c6adf7429e3
lordfm@163.com <lordfm@163.com@88a054c9-7ac1-712f-8f31-7815adbf56d5>
2012-02-25 05:27:51 +0000

    加入最小面积阈值

--- trunk/SRC/ImageProcess/Image2Vec.h
+++ trunk/SRC/ImageProcess/Image2Vec.h
@@ -9,9 +9,10 @@ namespace ImageProcess
 	* @param infile  输入影像
 	* @param outfile  输出矢量名
 	* @param classval  要进行矢量化的类别的像素值
+	* @param minisize  最小面积阈值，小于这个像素数目的区域将舍弃 
 	* @return 成功true, 失败false 
 	*/
-    IMP_DLL bool ImgClass2Shp(const char *infile,const char *outfile,unsigned char classval);
+    IMP_DLL bool ImgClass2Shp(const char *infile,const char *outfile,unsigned char classval,int minisize);
 
 }
 
