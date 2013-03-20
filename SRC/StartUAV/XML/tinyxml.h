ffe7c3a327b55c5b78854cbc0f75af17ea08b0c1
lordfm <lordfm@163.com>
2013-03-20 04:45:12 +0000

    修改工程界面，标绘工具为toolbox

--- SRC/StartUAV/XML/tinyxml.h
+++ SRC/StartUAV/XML/tinyxml.h
@@ -522,6 +522,7 @@ public:
 
 	const TiXmlNode* FirstChild()	const		{ return firstChild; }	///< The first child of this node. Will be null if there are no children.
 	TiXmlNode* FirstChild()						{ return firstChild; }
+	TiXmlNode* GetChildren() { return firstChild; }
 	const TiXmlNode* FirstChild( const char * value ) const;			///< The first child of this node with the matching 'value'. Will be null if none found.
 	/// The first child of this node with the matching 'value'. Will be null if none found.
 	TiXmlNode* FirstChild( const char * _value ) {
